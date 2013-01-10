74ab692a75839e3c3245cb8e40dcceba03db65ce
Martin Nilsson <martin@paperton.com>
2013-01-10 21:19:05 +0100

    First commit with all ze code.

---
+++ README.md
@@ -0,0 +1,22 @@
+# urbapp
+
+urbapp is just a simple CLI to help you quickly create apps on Urban Airship. Our iOS dev does this a lot so this will help him out. And it's always fun to automate stuff ;-)
+It returns App
+
+Example:
+	
+	./urbapp new --username myUsern0me --password P0ny --name "Awesome Pocahontas" --certificate /path/to/certificate.cer --certificate-password mySecretC3rt --headless
+
+Terminal output:
+
+	-------------------------------------------
+	App Key: kAWIDb29udniawd2
+	App Secret: kadin12dinaiwd9WAINOw
+	App Master Secret ajkwdnw91d012QWdwadAWDw
+	-------------------------------------------
+
+## Extra
+I've also included [headless](https://github.com/leonid-shevtsov/headless) so that if you are running an application using X (most apps on *nix, a few on OSX) it will run in the background.
+
+## License
+Do-exactly-what-you-want-with-it-I-do-not-care-license
---
+++ urbapp.rb
@@ -0,0 +1,71 @@
+require 'rubygems'
+require 'commander/import'
+require 'watir-webdriver'
+require 'headless'
+
+program :version, '0.0.1'
+program :description, 'CRUD Urban Airship Apps'
+ 
+command :new do |c|
+  c.syntax = 'urbapp new [options]'
+  c.summary = 'Create a new app'
+  c.description = 'Creates a new Urban Airship app using passed in options'
+  c.example 'description', 'new --username myUsern0me --password P0ny --name "Awesome Pocahontas" --certificate /path/to/certificate.cer --certificate-password mySecretC3rt --headless'
+  c.option '--username USERNAME', String, 'Urban Airship Username'
+  c.option '--password PASSWORD', String, 'Urban Airship Password'
+  c.option '--name NAME', String, 'Application name'
+  c.option '--certificate PATH', String, 'Path to certificate eg. /path/to/certificate.p12'
+  c.option '--certificate-password PASSWORD', String, 'Certificate password'
+  c.option '--headless', 'Run a headless browser using Xvfb'
+  c.action do |args, options|
+
+
+  		if options.headless
+  			headless = Headless.new
+  			headless = headless.start
+  		end
+
+		b = Watir::Browser.new :firefox
+
+		b.goto 'https://go.urbanairship.com/accounts/login/'
+
+		b.text_field(:id => 'id_username').set(options.username)
+		b.text_field(:id => 'id_password').set(options.password)
+		b.input(:xpath, "//input[@value='Log In']").click
+
+		b.goto 'https://go.urbanairship.com/apps/new'
+		b.text_field(:id => 'id_name').set(options.name)
+		b.select_list(:id => 'id_mode').select_value 'production'
+		b.select_list(:id => 'id_category').select 'Newsstand'
+
+		b.checkbox(:id => 'id_push_enabled').set
+
+		b.file_field(:id => 'id_certificate').set(options.certificate)
+		b.text_field(:id => 'id_certificate_password').set(options.certificate_password)
+
+		b.button(:value => 'Create your application').click
+
+		b.link(:id => 'app_secret_show').click
+		b.link(:id => 'app_master_secret_show').click
+		
+		b.link(:xpath, "//a[@id='app_master_secret_hide' and @style='display: inline;']").wait_until_present
+
+		app_key = b.dd(:id => 'app_key').text
+		app_secret = b.span(:id => 'app_secret').text
+		app_master_secret = b.span(:id => 'app_master_secret').text
+
+		say "-------------------------------------------"
+		say "App Key: #{app_key}"
+		say "App Secret: #{app_secret}"
+		say "App Master Secret: #{app_master_secret}"
+		say "-------------------------------------------"
+
+		begin
+			b.close
+			headless.destroy
+		rescue
+			# ;-)
+		end
+  end
+end
+

82b0691fa1337fccecf1d4883909c3c264f8a1d5
Martin Nilsson <martin@paperton.com>
2013-01-11 11:09:26 +0100

    Added --development mode. Updated docs.

--- README.md
+++ README.md
@@ -14,6 +14,47 @@ Outputs:
 	App Master Secret: ajkwdnw91d012QWdwadAWDw
 	-------------------------------------------
 
+./urbapp --help
+	
+	SYNOPSIS:
+
+		urbapp new [options]
+
+		DESCRIPTION:
+
+		Creates a new Urban Airship app using passed in options
+
+		EXAMPLES:
+			
+		# Production App with Headless browser
+		new --username myUsern0me --password P0ny --name "Awesome Pocahontas" --certificate /path/to/certificate.cer --certificate-password mySecretC3rt --headless
+			
+		# Development App
+		new --username myUsern0me --password P0ny --name "Awesome Pocahontas" --development
+			
+		OPTIONS:
+			
+		--username USERNAME 
+			Urban Airship Username
+			
+		--password PASSWORD 
+			Urban Airship Password
+			
+		--name NAME 
+			Application name
+			
+		--certificate PATH 
+			Path to certificate eg. /path/to/certificate.p12
+			
+		--certificate-password PASSWORD 
+			Certificate password
+			
+		--headless 
+			Run a headless browser using Xvfb
+			
+		--development 
+			Creates a development certificate, skip --certificate, --certificate-password
+
 ## Headless mode
 I've also included [headless](https://github.com/leonid-shevtsov/headless) so that if you are running an application using X (most apps on *nix, a few on OSX) it will run in the background.
 
--- urbapp.rb
+++ urbapp.rb
@@ -10,13 +10,15 @@ command :new do |c|
   c.syntax = 'urbapp new [options]'
   c.summary = 'Create a new app'
   c.description = 'Creates a new Urban Airship app using passed in options'
-  c.example 'description', 'new --username myUsern0me --password P0ny --name "Awesome Pocahontas" --certificate /path/to/certificate.cer --certificate-password mySecretC3rt --headless'
+  c.example 'Production App with Headless browser', 'new --username myUsern0me --password P0ny --name "Awesome Pocahontas" --certificate /path/to/certificate.cer --certificate-password mySecretC3rt --headless'
+  c.example 'Development App', 'new --username myUsern0me --password P0ny --name "Awesome Pocahontas" --development'
   c.option '--username USERNAME', String, 'Urban Airship Username'
   c.option '--password PASSWORD', String, 'Urban Airship Password'
   c.option '--name NAME', String, 'Application name'
   c.option '--certificate PATH', String, 'Path to certificate eg. /path/to/certificate.p12'
   c.option '--certificate-password PASSWORD', String, 'Certificate password'
   c.option '--headless', 'Run a headless browser using Xvfb'
+  c.option '--development', 'Creates a development certificate, skip --certificate, --certificate-password'
   c.action do |args, options|
 
 
@@ -35,13 +37,19 @@ command :new do |c|
 
 		b.goto 'https://go.urbanairship.com/apps/new'
 		b.text_field(:id => 'id_name').set(options.name)
-		b.select_list(:id => 'id_mode').select_value 'production'
-		b.select_list(:id => 'id_category').select 'Newsstand'
+
+		if options.development
+			b.select_list(:id => "id_mode").select_value 'development'
+			b.select_list(:id => 'id_category').select 'Newsstand'
+		else
+			b.select_list(:id => 'id_mode').select_value 'production'
+			b.select_list(:id => 'id_category').select 'Newsstand'
+
+			b.file_field(:id => 'id_certificate').set(options.certificate)
+			b.text_field(:id => 'id_certificate_password').set(options.certificate_password)
+		end
 
 		b.checkbox(:id => 'id_push_enabled').set
-
-		b.file_field(:id => 'id_certificate').set(options.certificate)
-		b.text_field(:id => 'id_certificate_password').set(options.certificate_password)
 
 		b.button(:value => 'Create your application').click
 
